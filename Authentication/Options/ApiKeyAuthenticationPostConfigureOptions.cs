892d62d9d29a48b2c563525064359d7ab75f49e2
moghaddam2005 <mahdimoghaddam2005@gmail.com>
2023-06-12 16:27:51 +0330

    Options of Post Requests For Authentication Scheme

--- Authentication/Options/ApiKeyAuthenticationPostConfigureOptions.cs
+++ Authentication/Options/ApiKeyAuthenticationPostConfigureOptions.cs
@@ -2,7 +2,7 @@ using ApiKey.Authentication.Options;
 using Microsoft.Extensions.Options;
 
 namespace ApiKet.Authentication.Options;
-
+// Configuring Post Requests
 public class ApiKeyAuthenticationPostConfigureOptions
     : IPostConfigureOptions<ApiKeyAuthenticationOptions>
 {
