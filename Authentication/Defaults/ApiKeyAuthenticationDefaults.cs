81154a8bd7fd27ca8ce60db38cb9f09c18fe6cf7
moghaddam2005 <mahdimoghaddam2005@gmail.com>
2023-06-12 16:25:15 +0330

    Authentication Scheme Default Name For Use Global

--- Authentication/Defaults/ApiKeyAuthenticationDefaults.cs
+++ Authentication/Defaults/ApiKeyAuthenticationDefaults.cs
@@ -2,5 +2,6 @@ namespace ApiKey.Authentication.Defaults;
 
 public static class ApiKeyAuthenticationDefaults
 {
+    // Default Scheme For ApiKey Services And Send (Like Authorization : ApiKey <KEY>)
     public const string AuthenticationScheme = "ApiKey";
 }
