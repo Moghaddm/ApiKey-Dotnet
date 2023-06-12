b613a0191bc9b8d50a296fb4f0e80d25204709ab
moghaddam2005 <mahdimoghaddam2005@gmail.com>
2023-06-12 16:27:10 +0330

    Options of Defined Scheme

--- Authentication/Options/ApiKeyAuthenticationOptions.cs
+++ Authentication/Options/ApiKeyAuthenticationOptions.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Authentication;
 
 namespace ApiKey.Authentication.Options;
-
+// I Dont Need To Options . Its For Large Scheme Systems
 public class ApiKeyAuthenticationOptions : AuthenticationSchemeOptions { }
