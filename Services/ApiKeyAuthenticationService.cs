1cec0e5fc249605638260b531965114d1be44a34
moghaddam2005 <mahdimoghaddam2005@gmail.com>
2023-06-12 16:29:02 +0330

    Implementing Logic For KEYs

--- Services/ApiKeyAuthenticationService.cs
+++ Services/ApiKeyAuthenticationService.cs
@@ -6,6 +6,7 @@ public class ApiKeyAuthenticationService : IApiKeyAuthenticationService
 {
     public Task<bool> IsValidAsync(string apiKey)
     {
+        // Set Your Logic For KEY !
         return Task.FromResult(apiKey == "test");
     }
 }
